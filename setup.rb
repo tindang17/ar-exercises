e3bfc53e94fc02c9023ae9bd0ac546be011b4189
Tin Trung Dang <tindang1710@gmail.com>
2017-06-06 13:01:20 -0700

    changed ar-exercises to ar-exercise because I wrote the datatbase name and it's easier this way. Successfully inserted 3 stores

--- setup.rb
+++ setup.rb
@@ -9,7 +9,7 @@ ActiveRecord::Base.logger = Logger.new(STDOUT)
 puts 'Establishing connection to database ...'
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
-  database: 'ar_exercises',
+  database: 'ar_exercise',
   username: 'development',
   password: 'development',
   host: 'localhost',
